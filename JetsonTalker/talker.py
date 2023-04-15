0ecf3a3256d083f8659e0b59c91c6ca7e94a821f
LoneX <elouanes.khelifi@uqtr.ca>
2023-04-14 23:36:54 -0400

    looping talker

--- JetsonTalker/talker.py
+++ JetsonTalker/talker.py
@@ -1,17 +1,13 @@
 import rospy
 from std_msgs.msg import String
 
-rospy.init_node('message_sender') # Initialize the node with a name
-
-# Create a publisher object that will publish messages to the '/my_topic' topic
+rospy.init_node('message_sender')
 pub = rospy.Publisher('/my_topic', String, queue_size=10)
 
-# Create a message object
-msg = String()
-msg.data = "Hello, world!"
-
-# Publish the message to the topic
-pub.publish(msg)
-
-# Spin the node to keep it running
-rospy.spin()
+rate = rospy.Rate(1) # publish once per second
+while not rospy.is_shutdown():
+    message = String()
+    message.data = "Hello, world!"
+    pub.publish(message)
+    rospy.loginfo("Published message: %s", message.data)
+    rate.sleep()
